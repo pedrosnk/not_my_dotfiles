28354272cce1dd45613e8f3ef7fe83eb30f4f57f
Pedro Medeiros <pedrosnk@gmail.com>
2016-05-10 07:33:05 -0300

    Add alias for bundle exec

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,9 @@ set -g OMF_PATH $HOME/.local/share/omf
 # Path to your oh-my-fish configuration.
 set -g OMF_CONFIG $HOME/.config/omf
 
+# set alias for bundle exec
+alias be='bundle exec'
+
 ### Configuration required to load oh-my-fish ###
 # Note: Only add configurations that are required to be set before oh-my-fish is loaded.
 # For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
