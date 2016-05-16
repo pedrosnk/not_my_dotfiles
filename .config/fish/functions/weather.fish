ea48a1575a0768f026ee78a32cce777f5a01f98c
Pedro Medeiros <pedrosnk@gmail.com>
2016-05-16 19:55:22 -0300

    Refine function that shows weather info

--- .config/fish/functions/weather.fish
+++ .config/fish/functions/weather.fish
@@ -1,3 +1,7 @@
-function weather
-  curl -4 wttr.in/$argv
+function weather -d "shows the weather to a given city eg. Rio de Janeiro"
+  if count $argv > /dev/null
+    curl -4 "wttr.in/$argv"
+  else
+    curl -4 "wttr.in/Rio de Janeiro"
+  end
 end
