a1739b2dab1b146ade0dafd5200c74c347e56d11
Pedro Medeiros <pedrosnk@gmail.com>
2016-05-17 10:44:42 -0300

    Dont generate pyc. just dont

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,9 @@ set -g OMF_CONFIG $HOME/.config/omf
 # set alias for bundle exec
 alias be='bundle exec'
 
+# dont generate pyc
+set -gx PYTHONDONTWRITEBYTECODE false
+
 ### Configuration required to load oh-my-fish ###
 # Note: Only add configurations that are required to be set before oh-my-fish is loaded.
 # For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
