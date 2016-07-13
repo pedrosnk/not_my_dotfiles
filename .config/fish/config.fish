26ce1570314edff8fe611ce3e2cd94b83a84cebb
Pedro Medeiros <pedrosnk@gmail.com>
2016-07-13 13:37:54 -0300

    remove comentary on .fish files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,11 +10,6 @@ alias be='bundle exec'
 # dont generate pyc
 set -gx PYTHONDONTWRITEBYTECODE false
 
-### Configuration required to load oh-my-fish ###
-# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
-# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
-# to create a custom plugin instead.
-
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
