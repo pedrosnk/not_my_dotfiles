ec53227ad50b1bcacd9d7f8eab9227272e991dc1
Pedro Medeiros <pedrosnk@gmail.com>
2023-07-11 10:23:22 -0300

    %s/'/"/g on lua files

--- .config/nvim/lua/pedrosnk/packer.lua
+++ .config/nvim/lua/pedrosnk/packer.lua
@@ -1,26 +1,26 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-  use('wbthomason/packer.nvim')
+  use("wbthomason/packer.nvim")
 
   use({
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    "nvim-telescope/telescope.nvim", tag = "0.1.1",
+    -- or                            , branch = "0.1.x",
+    requires = { {"nvim-lua/plenary.nvim"} }
   })
 
   use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
+    "rose-pine/neovim",
+    as = "rose-pine",
     config = function()
       require("rose-pine").setup()
     end
   })
 
   -- TreeSitter configuration
-  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
   use("nvim-treesitter/playground")
 
   use("ThePrimeagen/harpoon")
@@ -29,28 +29,28 @@ return require('packer').startup(function(use)
 
   -- NvimTree and dev icons
   use("nvim-tree/nvim-tree.lua")
-  use('nvim-tree/nvim-web-devicons')
+  use("nvim-tree/nvim-web-devicons")
 
   use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      {"neovim/nvim-lspconfig"},             -- Required
+      {"williamboman/mason.nvim"},           -- Optional
+      {"williamboman/mason-lspconfig.nvim"}, -- Optional
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      {"hrsh7th/nvim-cmp"},         -- Required
+      {"hrsh7th/cmp-nvim-lsp"},     -- Required
+      {"hrsh7th/cmp-buffer"},       -- Optional
+      {"hrsh7th/cmp-path"},         -- Optional
+      {"saadparwaiz1/cmp_luasnip"}, -- Optional
+      {"hrsh7th/cmp-nvim-lua"},     -- Optional
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
+      {"L3MON4D3/LuaSnip"},             -- Required
+      {"rafamadriz/friendly-snippets"}, -- Optional
     }
   }
 end)
